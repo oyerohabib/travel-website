502882e3ed563d0d080dc1d8553952db51fd4388
oyerohabib <oyerohabib@yahoo.com>
2022-12-27 22:42:39 +0100

    feat: added back to top functionality

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ const navLink = document.getElementsByClassName("navigation")[0];
 menu.addEventListener("click", () => {
   navLink.classList.toggle("active");
 });
+
+const backToTop = document.getElementsByClassName("back-to-top")[0];
+window.addEventListener("scroll", () => {
+  window.scrollY < 400
+    ? backToTop.classList.add("hide")
+    : backToTop.classList.remove("hide");
+});
